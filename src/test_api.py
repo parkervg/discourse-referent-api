69af044da900bb28b6158dd0eccc252fcd918547
parkervg <parkervg5@gmail.com>
2022-03-12 19:40:28 -0500

    modifying origins

--- src/test_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-
-if __name__ == "__main__":
-    text = "Today I am going to plant a tree. I take my shovel so I can dig a hole first. Then, "
-    r = requests.get("http://127.0.0.1:8000/get_json_prediction/", json={"text": text})
-    print(json.dumps(json.loads(r.json()), indent=4))
